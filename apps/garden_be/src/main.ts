0834f888178fe55e2d3a26f4bd7a279789f99d65
Patrik <patrikludvik@gmail.com>
2026-01-07 07:07:14 +0100

    fix controller modul, removed duplicated init seed

--- apps/garden_be/src/main.ts
+++ apps/garden_be/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PlantsService } from './plants/services/plants.service';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
@@ -8,9 +7,7 @@ async function bootstrap() {
     app.enableCors();
     const globalPrefix = 'api';
     app.setGlobalPrefix(globalPrefix);
-    const plantsService = app.get(PlantsService);
-    await plantsService.seedPlants();
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 8080;
     await app.listen(port);
     Logger.log(`Application is running on: http://localhost:${port}`);
 }
--- apps/garden_be/src/plants/plants.module.ts
+++ apps/garden_be/src/plants/plants.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PlantsService } from './services/plants.service';
 import { Plant } from './entity/plant.entity';
+import { PlantsController } from './plants.controller';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Plant])],
+    controllers: [PlantsController],
     providers: [PlantsService],
     exports: [PlantsService],
 })
